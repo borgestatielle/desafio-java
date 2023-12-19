c986965125fb3d729f291e7ee468f152aef3edbe
borgestatielle <borgestatielle25.com>
2023-12-19 11:11:16 -0300

    implementa request e response

--- src/main/java/com/wakandaacademy/desafiojava/pessoa/api/PessoaRequest.java
+++ src/main/java/com/wakandaacademy/desafiojava/pessoa/api/PessoaRequest.java
@@ -1,5 +1,21 @@
 package com.wakandaacademy.desafiojava.pessoa.api;
 
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import com.wakandaacademy.desafiojava.endereco.domain.Endereco;
+import lombok.Value;
+
+@Value
 public class PessoaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
+	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
+	private List<Endereco> enderecos;
 
 }
